560602b6962d09f900d671aecc4298e7e431dbda
Lee Siang Yin <siangyin916@gmail.com>
2022-09-03 19:43:33 +0800

    npm i mongoose and connect mongodb

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config();
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
 
 app.get("/", (req, res) => {
 	res.send("hello");
@@ -15,6 +16,16 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-	console.log(`server running on ${port}`);
-});
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}...`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
