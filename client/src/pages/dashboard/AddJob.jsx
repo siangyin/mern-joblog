b68551fa829b125c99b6fedef9db4cf762951d6a
Lee Siang Yin <siangyin916@gmail.com>
2022-09-04 21:41:38 +0800

    basic get all jobs in backend

--- client/src/pages/dashboard/AddJob.jsx
+++ client/src/pages/dashboard/AddJob.jsx
@@ -27,10 +27,6 @@ const AddJob = () => {
 		handleChange({ name, value });
 	};
 
-		// const handleChange = (e) => {
-		// 	setValues({ ...values, [e.target.name]: e.target.value });
-		// };
-
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
