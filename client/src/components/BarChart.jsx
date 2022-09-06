99511db455eaabf5ed295bc4f8031ccbd77320b7
Lee Siang Yin <siangyin916@gmail.com>
2022-09-06 17:34:41 +0800

    add barchart and areachart with recharts pkg

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,4 +1,24 @@
-const BarChart = () => {
-	return <div>BarChart</div>;
+import {
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	ResponsiveContainer,
+} from "recharts";
+
+const BarChartComponent = ({ data }) => {
+	return (
+		<ResponsiveContainer width="100%" height={300}>
+			<BarChart data={data} margin={{ top: 50 }}>
+				<CartesianGrid strokeDasharray="3 3 " />
+				<XAxis dataKey="date" />
+				<YAxis allowDecimals={false} />
+				<Tooltip />
+				<Bar dataKey="count" fill="#65c1e6" barSize={75} />
+			</BarChart>
+		</ResponsiveContainer>
+	);
 };
-export default BarChart;
+export default BarChartComponent;
