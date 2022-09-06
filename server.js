cc63d3223b9ac3a59051cae3450c2e96dccec3d6
Lee Siang Yin <siangyin916@gmail.com>
2022-09-06 19:53:35 +0800

    build front-end application
    update json> "build-client": "cd client && npm run build",
    in terminal npm run build-client
    update in server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
 
+// to build frontend application 1/3 (only when ready to deploy)
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 // db & authenticateUser
 import connectDB from "./db/connect.js";
 
@@ -25,16 +30,26 @@ if (process.env.NODE_ENV !== "production") {
 
 // app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-	res.json({ msg: "hello" });
-});
+// app.get("/", (req, res) => {
+// 	res.json({ msg: "hello" });
+// });
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// to build frontend application 2/3 (only when ready to deploy)
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authUser, jobsRouter);
+
+// to build frontend application 3/3 (only when ready to deploy)
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 const start = async () => {
 	try {
